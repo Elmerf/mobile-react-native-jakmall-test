0093cbd957f9ac4e4d64a32c6d2047da9b5eea09
Elmer Fiqi <elmerfiqi@gmail.com>
2025-03-03 20:28:33 +0700

    init Project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,30 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { Tabs } from "expo-router";
+import { Home, User } from "lucide-react-native";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { Icon } from "@/components/ui/icon";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import "@/global.css";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GluestackUIProvider mode="light">
+      <Tabs screenOptions={{ headerShown: false, animation: "shift" }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => <Icon color={color} as={Home} />,
+          }}
+        />
+        <Tabs.Screen
+          name="about"
+          options={{
+            title: "About",
+            tabBarIcon: ({ color }) => <Icon color={color} as={User} />,
+          }}
+        />
+      </Tabs>
+    </GluestackUIProvider>
   );
 }
